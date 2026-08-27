a14cd9fe8466535c5638905d40858c503b6b4136
Artur <Prussia2017@gmail.com>
2026-08-27 10:04:55 -0400

    Commit for August 27

---
+++ src/StudentInfo.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class StudentInfo {
+    //Every java project will have only a single main method
+    //Method = groping of relevant code +
+    // Return_Type (void) METHOD_NAME (main) (PARAM_Type PARAM_Name){
+    // RELEVANT CODES
+    //}
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+
+    }
+}
